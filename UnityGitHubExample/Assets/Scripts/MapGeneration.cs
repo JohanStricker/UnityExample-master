0dd122308d5a87764027f20ba5ce8560d8796252
JohanStricker <jdestr12@student.aau.dk>
2015-07-31 11:13:27 +0200

    adjusted GameMaster(GM) possition + added variables to mapgeneration

--- UnityGitHubExample/Assets/Scripts/MapGeneration.cs
+++ UnityGitHubExample/Assets/Scripts/MapGeneration.cs
@@ -3,6 +3,21 @@ using System.Collections;
 
 public class MapGeneration : MonoBehaviour {
 
+    //desired wall object to be initiated
+    public GameObject wall;
+    //possition of entire map(top left corner)
+    public Vector2 mapPossition = new Vector2(0, 0);
+    
+
+    //the static map
+   
+
+
+    //using awake to make sure that the map gets made as the absolute first thing
+    void Awake(){
+
+    }
+
 	// Use this for initialization
 	void Start () {
 	
