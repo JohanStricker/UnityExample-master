d65aebacf34cd41a279438195c504bc7b6979222
Daniel <dont@email.me>
2015-07-31 10:18:11 +0200

    Added penis. Press space for fun.

---
+++ UnityGitHubExample/Assets/Scripts/dostuff.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dostuff : MonoBehaviour {
+
+	public GameObject stuff;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Space))
+		{
+			GameObject g;
+			g = Instantiate(stuff, this.transform.position + this.transform.up * 1.5f, this.transform.rotation) as GameObject;
+			g.rigidbody.AddForce(this.transform.up * 5);
+		}
+	}
+}
