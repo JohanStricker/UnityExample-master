cd42c6c3201e6824c84eb10ab565f3a59f2ca0cd
JohanStricker <jdestr12@student.aau.dk>
2015-08-01 16:33:17 +0200

    sourcestree jump

---
+++ UnityGitHubExample/Assets/Scripts/Timer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class Timer{
+
+    float _resetTime;
+    float _durationTime;
+    int _maxTicks;
+    bool _singleTick;
+
+    public Timer(bool sT, float dT, int mT/*,do method*/) {
+        _durationTime = dT;
+        _resetTime = _durationTime;
+        _singleTick = sT;
+        _maxTicks = mT;
+    }
+
+    public void RunTimer(){
+
+        if (_durationTime <= 0.0f){
+            
+            //do method
+
+            //run once or multiple times at
+            if (!_singleTick){
+                _durationTime = _resetTime;
+
+            }
+        }
+    }
+
+
+}
---
+++ UnityGitHubExample/Assets/Scripts/Unit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Unit
+{
+
+    public Unit(/*unit specs*/)
+    {
+
+
+        //
+        
+
+    }
+
+}
+
